cb94221cf542f0c7aa3336f7a69fc130c2d51138
Jacob <jacobdumontier103@gmail.com>
2020-04-01 21:41:21 -0500

    Added a camera folder and a barcode test program

---
+++ code/Camera/barcodetest.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# import the necessary packages
+from pyzbar import pyzbar
+import argparse
+import cv2
+import os,sys
+import picamera
+import time
+
+# initialise pi camera
+if os.path.exists('/dev/video0') == False:
+  path = 'sudo modprobe bcm2835-v4l2 '
+  os.system (path)
+  time.sleep(1)
+
+cv2.namedWindow('Frame')
+vs = cv2.VideoCapture(0)
+vs.set(3, 1024)
+vs.set(4, 768)
+time.sleep(1.0)
+
+while True:
+   ok, image = vs.read()
+# loop over the detected barcodes
+   barcodes = pyzbar.decode(image)
+   print (barcodes)
+   for barcode in barcodes:
+	# extract the bounding box location of the barcode and draw the
+	# bounding box surrounding the barcode on the image
+      (x, y, w, h) = barcode.rect
+      cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+ 
+	# the barcode data is a bytes object so if we want to draw it on
+	# our output image we need to convert it to a string first
+      barcodeData = barcode.data.decode("utf-8")
+      barcodeType = barcode.type
+ 
+	# draw the barcode data and barcode type on the image
+      text = "{} ({})".format(barcodeData, barcodeType)
+      cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 0, 255), 2)
+ 
+	# print the barcode type and data to the terminal
+      print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
+ 
+# show the output image
+   cv2.imshow("Image", image)
+   cv2.waitKey(10)
