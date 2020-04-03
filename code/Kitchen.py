e3d26212d5dd10a1b2f42f4a5dec4346529eb970
Ndymario <ndymario@gmail.com>
2020-04-03 16:02:00 -0500

    Cleaned up the class a bit

--- code/Kitchen.py
+++ code/Kitchen.py
@@ -1,15 +1,13 @@
 class Item(object):
     def __init__(self, name):
         self.name = name
-        self.exporationDate = None
 
 class Perishable(Item):
-    def __init__(self, name):
+    def __init__(self, name, experationDate):
         Item.__init__(self, name)
-        self.exporationDate = True
+        self.experationDate = experationDate
 
 class NonPerishable(Item):
     def __init__(self, name):
         Item.__init__(self, name)
-        self.exporationDate = False
         
